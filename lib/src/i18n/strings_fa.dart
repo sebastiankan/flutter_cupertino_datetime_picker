20cfb4a76802f15fcfc7dfbb5dbd12e1e8046536
Sebastian <46951267+sebastiankan@users.noreply.github.com>
2021-08-09 13:44:48 +0430

    Create strings_fa.dart

---
+++ lib/src/i18n/strings_fa.dart
@@ -0,0 +1,78 @@
+part of 'date_picker_i18n.dart';
+
+/// English (EN) United States
+class _StringsFa extends _StringsI18n {
+  const _StringsFa();
+
+  @override
+  String getCancelText() {
+    return 'لغو';
+  }
+
+  @override
+  String getDoneText() {
+    return 'تایید';
+  }
+
+  @override
+  List<String> getMonths() {
+    return [
+      'دی',
+      'بهمن',
+      'اسفند',
+      'فروردین',
+      'اردیبهشت',
+      'خرداد',
+      'تیر',
+      'مرداد',
+      'شهریور',
+      'مهر',
+      'آبان',
+      'آذر'
+    ];
+  }
+
+  @override
+  List<String> getMonthsShort() {
+    return [
+      'دی',
+      'بهمن',
+      'اسفند',
+      'فروردین',
+      'اردیبهشت',
+      'خرداد',
+      'تیر',
+      'مرداد',
+      'شهریور',
+      'مهر',
+      'آبان',
+      'آذر'
+    ];
+  }
+
+  @override
+  List<String> getWeeksFull() {
+    return [
+      'دوشنبه',
+      'سه شنبه',
+      'چهارشنبه',
+      'پنج شنبه',
+      'جمعه',
+      'شنبه',
+      'یکشنبه'
+    ];
+  }
+
+  @override
+  List<String> getWeeksShort() {
+    return [
+      'دوشنبه',
+      'سه شنبه',
+      'چهارشنبه',
+      'پنج شنبه',
+      'جمعه',
+      'شنبه',
+      'یکشنبه'
+    ];
+  }
+}
